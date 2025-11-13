435e3759d7d7167340280d7279257e528b50ec17
Craig Loewen <crloewen@microsoft.com>
2025-11-13 13:22:15 -0500

    Init commit

--- src/world.rs
+++ src/world.rs
@@ -39,6 +39,6 @@ pub struct WorldPlugin;
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_world)
+        app.add_systems(Startup, setup_world);
     }
 }
